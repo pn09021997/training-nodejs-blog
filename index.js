4497fd0bb97fd6c5586f6b092ccff3fa92867e65
pn09021997 <pn09021997@odoo.com>
2023-09-25 08:52:31 +0700

    add morgan package

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
-app.get('/tin-tuc', (req, res) => {
-    var a = 1
-    var b = 2
+app.use(morgan('combined'))
 
-    var c = a + b
+app.get('/tin-tuc', (req, res) => {
     return res.send('Hello World!')
 })
 
